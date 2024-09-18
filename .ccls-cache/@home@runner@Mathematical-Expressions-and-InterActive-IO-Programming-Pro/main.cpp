304cbacf8d9573c89c29467f7b6124517623e6fd
zwhippo9 <zwhippo9@gmail.com>
2024-09-18 02:49:14 +0000

    1

--- .ccls-cache/@home@runner@Mathematical-Expressions-and-InterActive-IO-Programming-Pro/main.cpp
+++ .ccls-cache/@home@runner@Mathematical-Expressions-and-InterActive-IO-Programming-Pro/main.cpp
@@ -1,3 +1,27 @@
+// description
 #include <iostream>
+using namespace std;
 
-int main() { std::cout << "Hello World!\n"; }
+int main(){
+  
+//constants
+  const float sugar_cups = 1.5;
+  const int butter_cups = 1;
+  const float flower_cups = 2.75;
+  const int cookies_recipe = 48;
+
+//get # of cookies from user
+  float(input "Enter number of cookies: ") = num_of_cookies
+
+//calulations
+  float sugar_needed = (num_of_cookies / cookies_recipe) * sugar_cups;
+  float butter_needed = (num_of_cookies / cookies_recipe) * butter_cups;
+  float flour_needed = (num_of_cookies / cookies_recipe) * flower_cups;
+
+//Display
+  print(f"To make {num_of_cookies} cookies, you will need: "))
+  print(f"{sugar_needed} cups of sugar")
+  print(f"{butter_needed} cups of butter")
+  print(f"{flour_needed} cups of flour")
+
+  
