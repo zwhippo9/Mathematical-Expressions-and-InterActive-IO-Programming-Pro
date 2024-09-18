3b95cc23f5333023b84a4d52d1094ed78dde7094
zwhippo9 <zwhippo9@gmail.com>
2024-09-18 20:13:14 +0000

    2

--- .ccls-cache/@home@runner@Mathematical-Expressions-and-InterActive-IO-Programming-Pro/main.cpp
+++ .ccls-cache/@home@runner@Mathematical-Expressions-and-InterActive-IO-Programming-Pro/main.cpp
@@ -1,3 +1,33 @@
 #include <iostream>
+#include <cmath> // for pow function
+int main() {
+//constants
+  double principle =0.0;
+  double rate;
+  double time;
+  double interest;
+  double amount;
 
-int main() { std::cout << "Hello World!\n"; }
+// display values to ueser
+  cout << "Enter the principle amount: ";
+  cin >> principle;
+  cout << "Enter the interest rate (as a decimal): ";
+  cin >> rate;
+  cout << "Enter the time in years: ";
+  cin >> time;
+
+// calculate amount
+  amount = principle * pow((1 + rate), time);
+  
+//calculate interest
+    interest = amount - principle;
+
+//Display to user
+  cout << "Interest rate: " << rate << endl;  
+  cout << "Time: " << time << endl;
+  cout << "Principle: " << principle << endl;
+  cout << "Total amount after interest: " << amount << endl;
+  cout << "Interest earned: " << interest << endl;
+
+return 0;
+}
